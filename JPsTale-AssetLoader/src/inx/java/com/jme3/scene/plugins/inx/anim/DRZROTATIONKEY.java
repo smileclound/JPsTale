86647bd26d7e24419267b431068a567301bd7d93
yan <115050813@qq.com>
2017-06-30 12:02:05 +0800

    删除旧的inx loader

--- JPsTale-AssetLoader/src/inx/java/com/jme3/scene/plugins/inx/anim/DRZROTATIONKEY.java
+++
@@ -1,8 +0,0 @@
-package com.jme3.scene.plugins.inx.anim;
-
-import com.jme3.math.Quaternion;
-
-public class DRZROTATIONKEY {
-	public long mTime;
-	public Quaternion mValue;
-}
--- JPsTale-AssetLoader/src/inx/java/com/jme3/scene/plugins/inx/anim/DRZSCALEKEY.java
+++
@@ -1,8 +0,0 @@
-package com.jme3.scene.plugins.inx.anim;
-
-import com.jme3.math.Vector3f;
-
-public class DRZSCALEKEY {
-	public long mTime;
-	public Vector3f mValue;
-}
